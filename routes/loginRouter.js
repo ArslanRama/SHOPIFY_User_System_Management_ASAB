fef888e63d0028893ce1e50a044d10852eb05b39
ArslanRama <arslanrama84@gmail.com>
2021-05-03 21:56:12 +0200

    hbs forms

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const loginController = require("../controllers/login");
+const login = require("../controllers/login");
 
-router.get("/", loginController.loginForm);
-router.post("/", loginController.loginWithUser);
-//! Login Admin
-router.get("/admin", loginController.adminLoggedIn);
-//! createUserAdmin
-router.post("/admin", loginController.createNewUser);
+router.get("/", login.loginForm);
+router.post("/", login.loginWithUser);
 
+//! ADMIN
+router.get("/admin", login.adminLoggedIn);
+// Create a New user  
+router.post("/admin", login.createNewUser);
+// Update User 
+router.get("/admin/update/:id", login.updateUser1);
+// Updated user
+router.post("/admin/update/:id", login.updatedUser);
+// Delete User 
+router.get("/admin/delete/:id", login.deleteUser);
 
-//! DeleteUser Admin
-router.get("/admin/delete/:id", loginController.deleteUser);
-//! Login User
-router.get("/user", loginController.loginUser);
-//! Add Product User
-router.post("/user", loginController.addProduct);
-//! Update Product User
-router.get("/user/update/:id", loginController.updateProduct);
-//! Delete Product User
-router.get("/user/delete/:id", loginController.deleteProduct);
+//! USER
+router.get("/user", login.loginUser);
+// Add Product User 
+router.post("/user", login.addProduct);
+// Update Product User
+router.get("/user/update/:id", login.updateProduct1);
+// Updated Product User
+router.post("/user/update/:id", login.updatedProduct);
+
+// Delete Product User
+router.get("/user/delete/:id", login.deleteProduct);
 module.exports = router;
