1e9e4169f6930d8cdace26213148937d00b9ad78
syed shah <moazzam.shah106@gmail.com>
2021-05-03 11:20:12 +0200

    made some changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,5 @@
-const User =require('../models/User')
-const bcrypt = require('bcrypt')
-
-exports.registerGet = (req, res)=>{
-    res.render('signup')
-}
-
-exports.registerPost = async(req , res)=>{
-    const { name , email, password ,address, country, salary} = req.body
-    let userData  = await User.findOne({email});
-    if(userData){
-       return res.status(400).json({ msg :"user already exist "})
-    }
-    const user = new User({
-        name, email , password ,address, country, salary
-    })
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password , salt )
-    // res.json(req.body)
-    user.save()
-    res.send(user)
-   
- }
+const homePage = (req, res) => {
+    res.render("home", { pageTitle: "Home Page" });
+  };
+  
+  module.exports = { homePage };
