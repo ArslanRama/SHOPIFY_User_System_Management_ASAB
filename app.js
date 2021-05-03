5db97f9298748c2398e39546d573b360078a131c
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-05-03 09:30:58 +0200

    added registeration part

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ const express = require('express');
 const app = express();
 const PORT= 8080;
 
+// MongoDB connection using mongoose module
+const DB_LINK = 'mongodb+srv://admin:admin@cluster0.fcrdg.mongodb.net/firstDB'
+const mongoose = require('mongoose')
+mongoose.connect(DB_LINK, {
+    useUnifiedTopology : true,
+    useNewUrlParser: true
+})
+.then(()=> console.log('MongoDB is succesfully connected '))
+.catch(()=> console.log('Database connection failed '))
+
+
+app.use(express.json()) 
+
 
 const indexRouter = require('./routes/indexRouter')
 
 
-
+app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'hbs');
 
+app.use(express.urlencoded({extended: false }))
 
 //! Routings
 app.use('/', indexRouter);
+
 
 //! Listen app with the port
 app.listen(PORT, ()=>{
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const router=express.Router();
+const router = express.Router();
+const indexController = require('../controllers/index')
+
 
 router.get('/', (req, res)=>{
     res.render('layout', {
         messsage: 'Hello world'
     })
 })
+
+router.get('/register',indexController.registerGet)
+
+router.post('/register',indexController.registerPost)
+
 
 module.exports = router
